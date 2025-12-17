a05462d96a727cb372efe470d7243c2f57a67811
phuuthanh-dev <phuuthanh2003@gmail.com>
2025-12-17 15:08:49 +0700

    Fix import path for Icons in TripsHeader component

--- src/presentation/features/tripListing/ui/orgamisms/TripsHeader.tsx
+++ src/presentation/features/tripListing/ui/orgamisms/TripsHeader.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
-import { Icon } from "../atoms/Icons/Icons";
+import { Icon } from "../atoms/icons/Icons";
 
 interface TripsHeaderProps {
     onRefresh: () => void;
